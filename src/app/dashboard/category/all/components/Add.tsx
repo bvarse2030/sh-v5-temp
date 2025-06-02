b7be9b47b0a7b7e9526e07f7503b51b8ca6e82d4
Toufiquer <toufiquerabc@gmail.com>
2025-06-02 19:22:45 +0600

    done @ category just name

--- src/app/dashboard/category/all/components/Add.tsx
+++ src/app/dashboard/category/all/components/Add.tsx
@@ -6,7 +6,7 @@
 |-----------------------------------------
 */
 
-import React, { useState } from 'react';
+import React from 'react';
 
 import { Input } from '@/components/ui/input';
 import { Label } from '@/components/ui/label';
